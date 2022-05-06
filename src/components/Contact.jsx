71737f60e5a16fdef14b05566c63ad4c5ac17c39
merzou_r <merzou_r@etna-alternance.net>
2022-05-06 15:47:59 +0200

    Portfolio
    Portfolio en cours

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div name='contact' className='w-full h-screen bg-[#404c73] flex justify-center items-center p-4'>
+        <form method='POST' action="https://getform.io/f/2d61f470-aa2d-4b60-bcb8-f434fa74c3c4" className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                <p className='text-gray-300 py-4'>Envoyez </p>
+            </div>
+            <input className='bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6]' name='message' rows='10' placeholder='Message'></textarea>
+            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex'>Collab</button>
+6        </form>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Skills.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import GitHub from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
+import sql from '../assets/sql.png';
+import react from '../assets/react.png'
+import laravel from '../assets/laravel.png';
+import node from '../assets/node.png';
+
+
+
+
+const Skills = () => {
+  return (
+    <div name="skills" className='w-full h-screen bg-[#192135] text-gray-300'>
+      {/*Container*/}
+
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div>
+          <p className='text-4xl font-bold inline border-b-4 border-pink-600' >Experience</p>
+          <p className='py-4'> Voici les technologies dont j'ai utilisé pour faire des projets.</p>
+        </div>
+
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={HTML} alt='icon html' />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={CSS} alt='icon html' />
+            <p className='my-4'>CSS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={sql} alt='icon html' />
+            <p className='my-4'></p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Tailwind} alt='icon html' />
+            <p className='my-4'>Tailwind</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={JavaScript} alt='icon html' />
+            <p className='my-4'>JavaScript</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={react} alt='icon html' />
+            <p className='my-4'>React</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={GitHub} alt='icon html' />
+            <p className='my-4'>Git</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={laravel} alt='icon html' />
+            <p className='my-4'>Laravel</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={node} alt='icon html' />
+            <p className='my-4'>NodeJS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
