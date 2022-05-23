bed2a4f4f59c2d31935c4c0fc4f33f3e0a2cd3db
Ramzi <merzou_r@etna-alternance.net>
2022-05-23 14:09:53 +0200

    Update Navbar.jsx
    lien github en ligne

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
                  </a>
             </li>
             <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                 <a className='flex justify-between items-center w-full text-gray-300' href="/">
+                 <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/merzouur/">
                      GitHub <FaGithub size={30} /> 
                  </a>
             </li>
