60fff5c2f24b6bd810e934254acf8933cd47fee8
merzou_r <merzou_r@etna-alternance.net>
2022-05-30 09:44:31 +0200

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/2d61f470-aa2d-4b60-bcb8-f434fa74c3c4" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Envoyez </p>
+                <p className='text-gray-300 py-4'>Contactez moi en cas de besoin ! </p>
             </div>
             <input className='bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
