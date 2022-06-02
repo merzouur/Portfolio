f1a73a8fa23d34f9835eabe62523f8f88da0d3de
merzou_r <merzou_r@etna-alternance.net>
2022-06-02 09:49:09 +0200

    update react movies deployé et integré dans le portfolio

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import WorkImg from '../assets/workImg.jpeg'
 import realEstate from '../assets/realestate.jpg'
 import CLickerGame from '../assets/ClickerGame.png'
+import movies from '../assets/movies.png'
 
 const Work = () => {
   return (
@@ -30,17 +31,17 @@ const Work = () => {
                         </div>
                     </div>
                 </div>
-                <div  style={{ backgroundImage: `url(${WorkImg})` }}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div  style={{ backgroundImage: `url(${movies})` }}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     {/* Hover Effect */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'> React Movies
 
                         </span>
                         <div className='pt-0 text-center'>
-                            <a href="/">
+                            <a href="https://merzouur.github.io/movies">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href='/'>
+                            <a href='https://github.com/merzouur/movies'>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
 
